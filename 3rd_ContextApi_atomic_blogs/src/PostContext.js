2e59b85932b0bd20069901cf35a4a0275a39f732
Darshanpro123 <darshanpro187@gmail.com>
2024-10-23 10:37:26 +0530

    Add 3rd Project

---
+++ 3rd_ContextApi_atomic_blogs/src/PostContext.js
@@ -0,0 +1,60 @@
+import { createContext, useContext, useState } from "react";
+import { faker } from "@faker-js/faker";
+
+const createRandomPost = () => {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  };
+};
+
+//create new context
+const PostContext = createContext();
+
+const PostProvider = ({ children }) => {
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 32 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  const handleAddPost = (post) => {
+    setPosts((posts) => [post, ...posts]);
+  };
+
+  const handleClearPosts = () => {
+    setPosts([]);
+  };
+
+  return (
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        searchQuery,
+        setSearchQuery,
+        onAddPost: handleAddPost,
+        onClearPosts: handleClearPosts,
+      }}
+    >
+      {children}
+    </PostContext.Provider>
+  );
+};
+
+const usePosts = () => {
+  const context = useContext();
+  if (!context) {
+    throw new Error("usePosts must be used within a PostProvider");
+  }
+  return context;
+};
+export { PostProvider, usePosts };

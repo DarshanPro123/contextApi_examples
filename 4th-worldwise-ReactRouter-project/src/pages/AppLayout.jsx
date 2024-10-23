08f97b73308164fd12db3e5285d5ff85072e4bca
Darshanpro123 <darshanpro187@gmail.com>
2024-10-23 18:56:06 +0530

    Edit all pages and navigate all pages

---
+++ 4th-worldwise-ReactRouter-project/src/pages/AppLayout.jsx
@@ -0,0 +1,21 @@
+// import Map from "../components/Map";
+// import Sidebar from "../components/Sidebar";
+// import User from "../components/User";
+
+import styles from "./AppLayout.module.css";
+
+function AppLayout() {
+  return (
+    <div>
+      {/* <div className={styles.app}>  */}
+
+      <h1>hi that is app</h1>
+      {/* <Sidebar />
+
+      <Map />
+      <User /> */}
+    </div>
+  );
+}
+
+export default AppLayout;
